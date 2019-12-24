f2809eecec00ef72f5f657d25d41edf827327007
Peter Ngo <peterngo2496@gmail.com>
2019-12-24 18:13:36 -0500

    added a check cards function that checks

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 # main functions of program will go in here
 import random
 
+# this function will check if the cards equals to 21
+# TODO: Why doesn't this work?
+def checkCards(hand):
+    print(hand)
+    for i in hand:
+        if (i == "J" or i == "K" or i == "Q"):
+            i = 10
+        if (i == "A"):
+            i = 11
+
+    if(hand[0] + hand[1] == 21):
+        print("you have won!")
+
 # this function will deal the cards
 def dealCards(cards):
     hand = []
@@ -22,7 +35,7 @@ def dealCards(cards):
         else:
             hand.append(dealCard)
 
-    print(hand)
+    return(hand)
 
 
 # this is going to start the game
@@ -30,9 +43,14 @@ def start():
     # lets start with the deck itself
     # going to use an array to hold the values of the cards
 
+    # when you start the game define a deck of cards
+    # TODO: this is not a full deck, make it a full deck with 52 cards
     cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
-    #random.shuffle(cards)
-    dealCards(cards)
+    playerhand = dealCards(cards)
+    cpuhand = dealCards(cards)
+    checkCards(playerhand)
+    print("Your cards: ", playerhand)
+    #print(cpuhand)
 
     print("lets play some blackjack dog")
 
